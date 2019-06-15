23fe82b6644b552164d1017b0e494613e62a5af0
Marcin Ruszkiewicz <marcin.ruszkiewicz@polcode.net>
2019-06-15 16:29:02 +0200

    fix require

--- lib/i2c_drivers.rb
+++ lib/i2c_drivers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'i2c_drivers/version'
-require 'i2c_drivers/driver/base'
+require 'i2c_drivers/driver/driver'
 require 'i2c_drivers/driver/i2c_dev'
 
 module I2CDrivers
